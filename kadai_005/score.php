26b001cfaa4818a6ad34cfd26f5e0271e972fc58
YasuTaka-K <167872018+YasuTaka-K@users.noreply.github.com>
2024-08-12 00:08:44 +0900

    Add php/kadai_005

---
+++ kadai_005/score.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP 基礎練習</title>
+  </head>
+
+  <body>
+    <p>
+      <?php 
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40; 
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+        
+        $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        $average = $total / 10;
+
+        echo "合計:" . $total . "<br>";
+        echo "平均:" . $average ."<br>";
+      ?>
+    </p>  
+  </body>
+</html>
