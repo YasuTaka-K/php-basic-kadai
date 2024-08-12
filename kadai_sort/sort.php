17090901f21b668da11233a1f6e8a729c3ec2266
YasuTaka-K <167872018+YasuTaka-K@users.noreply.github.com>
2024-08-12 13:57:36 +0900

    Add php kadai_sort

---
+++ kadai_sort/sort.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10];
+
+  // 昇順・降順の関数
+        function sort_2way(&$array, $order) {
+          if($order === TRUE) {
+            echo "昇順にソートします。" . "<br>";
+            sort($array);
+          } else if($order === FALSE) {
+            echo "降順にソートします。" . "<br>";
+            rsort($array);
+          }
+        }
+
+        // 昇順を画面に表示
+        sort_2way($nums, TRUE);
+        foreach ($nums as $num) {
+          echo $num . "<br>";
+        }
+        // 降順を画面に表示
+        sort_2way($nums, FALSE);
+        foreach ($nums as $num) {
+          echo $num . "<br>";
+        }
+        ?>
+    </p>
+</body>
+
+</html>
